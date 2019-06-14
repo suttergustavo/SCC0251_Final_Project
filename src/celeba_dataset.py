4be8fb62ad7c6b1f39452ee452c75f004a7f3200
Gustavo <gsutter98@gmail.com>
2019-06-14 15:47:42 -0300

    Model and pytorch dataset added

---
+++ src/celeba_dataset.py
@@ -0,0 +1,29 @@
+class CelebADataset(Dataset):
+  def __init__(self, root_dir, factor, n_samples):
+    self.root_dir = root_dir
+    self.n_samples = n_samples
+    self.factor = factor
+    
+  def __len__(self):
+    return self.n_samples
+  
+  def __getitem__(self, idx):
+    img_name = os.path.join(self.root_dir, f'{idx:06d}.jpg')
+    
+    original_image =Image.open(img_name)
+    
+    
+    img_shape = list(original_image.size)
+    img_shape[0], img_shape[1] = img_shape[1], img_shape[0]
+    
+    tsfrm = transforms.Compose([
+        transforms.Resize((int(img_shape[0] / self.factor), int(img_shape[1] / self.factor)), interpolation=2),
+        transforms.Resize(img_shape, interpolation=2),
+        transforms.ToTensor()
+    ])
+    
+    
+    degrated_image = tsfrm(original_image)
+    original_image = transforms.ToTensor()(original_image)
+    
+    return degrated_image, original_image
