cb11527cad067f7c3964cbab72ae1377926b5cfd
Gustavo <gsutter98@gmail.com>
2019-06-18 09:24:50 -0300

    Added code to track loss history

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,13 +11,11 @@ use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 train_folder = '../data/train'
-val_folder = '../data/val'
 batch_size = 64
 epochs = 20
 learning_rate = 1e-3
 
 train_set = CelebADataset(root_dir=train_folder, factor=2, n_samples=5000)
-# val_set = CelebADataset(root_dir=val_folder, factor=2, n_samples=1000)
 
 train_loader = DataLoader(train_set, batch_size=batch_size)
 
@@ -24,7 +23,11 @@ net = Net().to(device)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters())
 
+loss_history = []
+
 for epoch in range(epochs):
+    running_loss = 0
+    count = 0
     for i, (inputs, targets) in enumerate(train_loader):
         inputs, targets = inputs.to(device), targets.to(device)
 
@@ -36,7 +39,14 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
 
-    print(f'Epoch {epoch} - Loss: {loss.item()}')
+        running_loss += loss.item()
+        count += 1
 
+    print(f'Epoch {epoch} - Loss: {loss.item()}')
+    loss_history.append(running_loss/count)
+
+
+loss_history = np.array(loss_history)
+np.save('loss_history.npy', loss_history)
 
 torch.save(net.state_dict(), 'weights.pth')
